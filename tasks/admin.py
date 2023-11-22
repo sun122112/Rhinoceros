88987b4d1d94916d05b9a00daf2afd7ceca440b2
Abigail Tang <abigail.tang@kcl.ac.uk>
2023-11-22 18:53:57 +0000

    Configure admin interface for Users.

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
+'''Configuration of the admin interface for this project.'''
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of admin interface for users."""
+    # specify attributes included in table view of users
+    list_display = [
+        'username', 'first_name', 'last_name' , 'is_active'
+    ]
