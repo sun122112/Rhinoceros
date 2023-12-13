d38b1acd8590c0888a80d26d647b010a661ead00
Ka (Ka Wai) Lau <kwlau107@gmail.com>
2023-12-13 03:39:43 +0000

    Add test and fixtures for delete team task

--- tasks/tests/views/test_delete_task_view.py
+++ tasks/tests/views/test_delete_task_view.py
@@ -7,28 +7,46 @@ from tasks.models import Task, User
 class DeleteTaskViewTestCase(TestCase):
     """Tests of the delete task view."""
 
-    fixtures = ['tasks/tests/fixtures/default_user.json', 'tasks/tests/fixtures/tasks.json']
+    fixtures = [
+        'tasks/tests/fixtures/default_user.json',
+        'tasks/tests/fixtures/tasks.json',
+        'tasks/tests/fixtures/task_with_team.json',
+        'tasks/tests/fixtures/teams.json'
+    ]
 
     def setUp(self):
         self.user = User.objects.get(username='@johndoe')
         self.client.login(username=self.user.username, password='Password123')
-        self.task = Task.objects.get(pk=1)
-        self.url = reverse('delete_task', kwargs={'task_id': self.task.id})
+        self.task_without_team = Task.objects.get(pk=1)
+        self.task_with_team = Task.objects.get(pk=2)
+        self.url_without_team = reverse('delete_task', kwargs={'task_id': self.task_without_team.id})
+        self.url_with_team = reverse('delete_task', kwargs={'task_id': self.task_with_team.id})
 
-    def test_delete_task_url(self):
-        expected_url = f'/dashboard/delete_task/{self.task.id}/'
-        self.assertEqual(self.url, expected_url)
+    def test_delete_task_without_team_url(self):
+        expected_url = f'/dashboard/delete_task/{self.task_without_team.id}/'
+        self.assertEqual(self.url_without_team, expected_url)
 
-    def test_get_delete_task(self):
-        response = self.client.get(self.url)
+    def test_delete_task_with_team_url(self):
+        expected_url = f'/dashboard/delete_task/{self.task_with_team.id}/'
+        self.assertEqual(self.url_with_team, expected_url)
+
+    def test_get_delete_task_without_team(self):
+        response = self.client.get(self.url_without_team)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'delete_task.html')
         task = response.context['task']
-        self.assertEqual(task, self.task)
+        self.assertEqual(task, self.task_without_team)
 
-    def test_successful_delete_task(self):
+    def test_get_delete_task_with_team(self):
+        response = self.client.get(self.url_with_team)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'delete_task.html')
+        task = response.context['task']
+        self.assertEqual(task, self.task_with_team)
+
+    def test_successful_delete_task_without_team(self):
         before_count = Task.objects.count()
-        response = self.client.post(self.url, follow=True)
+        response = self.client.post(self.url_without_team, follow=True)
         after_count = Task.objects.count()
         self.assertEqual(after_count, before_count-1)
         response_url = reverse('my_tasks')
@@ -36,4 +54,15 @@ class DeleteTaskViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'my_tasks.html')
         messages = list(response.context['messages'])
         self.assertEqual(str(messages[0]), "Task Deleted!")
+
+    def test_successful_delete_task_with_team(self):
+        before_count = Task.objects.count()
+        response = self.client.post(self.url_with_team, follow=True)
+        after_count = Task.objects.count()
+        self.assertEqual(after_count, before_count-1)
+        response_url = reverse('team_info', kwargs={'team_id': self.task_with_team.team_id})
+        self.assertRedirects(response, response_url, status_code=302, target_status_code=200)
+        self.assertTemplateUsed(response, 'team_info.html')
+        messages = list(response.context['messages'])
+        self.assertEqual(str(messages[0]), "Team Task Deleted!")
 
